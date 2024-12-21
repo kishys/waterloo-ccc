3d21efa75befdea4c0ec1c7a6443951b3e1de616
kishys <kishansuhir@gmail.com>
2024-12-20 23:30:48 -0500

    j1 '20

---
+++ 2020/jr/J1 - Dog Treats.py
@@ -0,0 +1,13 @@
+# Kishan Suhi
+# CCC - '20 J1: Dog Treats
+
+a = int(input(""))
+b = int(input(""))
+c = int(input(""))
+
+scale = a + b*2 + c*3
+
+if scale >= 10:
+    print("happy")
+else:
+    print("sad")
