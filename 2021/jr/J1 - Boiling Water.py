e85eb8e4b03d26837d8709d58fdc1d6962571d11
kishys <kishansuhir@gmail.com>
2024-12-20 23:29:53 -0500

    j1 -21

---
+++ 2021/jr/J1 - Boiling Water.py
@@ -0,0 +1,14 @@
+# Kishan Suhi
+# CCC - '21 J1: Boiling Water
+
+a = int(input(""))
+
+pressure = 5 * a - 400
+
+print(pressure)
+if pressure > 100:
+    print("-1")
+elif pressure < 100:
+    print("1")
+else:
+    print("0")
