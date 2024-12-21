8e4b44ab26dc77d10f0e6fbeda22ecd22375828c
kishys <kishansuhir@gmail.com>
2024-12-20 23:31:34 -0500

    j1 '18

---
+++ 2018/jr/J1 - Telemarketer or not.py
@@ -0,0 +1,12 @@
+# Kishan Suhi
+# CCC - '18 J1: Telemarketer or not?
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+if (a == 8 or a == 9) and (d == 8 or d == 9) and (b == c):
+    print("ignore")
+else:
+    print("answer")
